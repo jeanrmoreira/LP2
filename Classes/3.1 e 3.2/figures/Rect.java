88b433fdab4d8b56e20ba5883d375192a51e4a60
jeanrmoreira <48541470+jeanrmoreira@users.noreply.github.com>
2021-03-21 03:24:23 -0300

    Create Rect.java

---
+++ Classes/3.1 e 3.2/figures/Rect.java
@@ -0,0 +1,33 @@
+package figures;
+
+import java.awt.*;
+
+public class Rect {
+   private int x, y;
+   private int w, h;
+   private Color contorno;
+   private Color fundo;
+
+    public Rect (int x, int y, int w, int h, Color contorno, Color fundo) {
+        this.x = x;
+        this.y = y;
+        this.w = w;
+        this.h = h;
+        this.contorno =  contorno;
+        this.fundo = fundo;
+    }
+
+    public void print () {
+        System.out.format("Retangulo de tamanho (%d,%d) na posicao (%d,%d).\n",
+            this.w, this.h, this.x, this.y);
+    }
+
+    public void paint (Graphics g) {
+        Graphics2D g2d = (Graphics2D) g;
+        
+        g2d.setColor(fundo);
+        g2d.fillRect(this.x,this.y, this.w,this.h);
+        g2d.setColor(contorno);
+        g2d.drawRect(this.x,this.y, this.w,this.h);
+    }
+}
