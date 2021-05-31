e9e1349dbdd5868d3252cd0c26fd2a1e48b048dc
jeanrmoreira <48541470+jeanrmoreira@users.noreply.github.com>
2021-05-30 21:20:50 -0300

    Update ListApp.java

--- Projeto/ListApp.java
+++ Projeto/ListApp.java
@@ -3,6 +3,7 @@ import java.awt.event.*;
 import javax.swing.*;
 import java.util.ArrayList;
 import java.util.Random;
+import java.io.*;
 import java.awt.geom.Ellipse2D;
 import java.awt.geom.Ellipse2D.Double;
 import figures.*;
@@ -18,20 +19,48 @@ class ListApp {
 
 class ListFrame extends JFrame {
     ArrayList<Figure> figs = new ArrayList<Figure>();
+    ArrayList<Button> buts = new ArrayList<Button>();
     Random rand = new Random();
-    
+
+Button focus_but = null;    
 Figure focus = null;
 Point ponto;
+Point pos = null;
+
 
    
     ListFrame () {
+    
+    try {
+            FileInputStream f = new FileInputStream("proj.bin");
+            ObjectInputStream o = new ObjectInputStream(f);
+            this.figs = (ArrayList<Figure>) o.readObject();
+            o.close();
+        } catch (Exception x) {
+            System.out.println("ERRO!");
+        }
+        
         this.addWindowListener (
             new WindowAdapter() {
                 public void windowClosing (WindowEvent e) {
-                    System.exit(0);
+                        try {
+                            FileOutputStream f = new FileOutputStream("proj.bin");
+                            ObjectOutputStream o = new ObjectOutputStream(f);
+                            o.writeObject(figs);
+                            o.flush();
+                            o.close();
+                        } catch (Exception x) {
+                        }
+                        System.exit(0);
+                    }
                 }
-            }
-        ); 
+        );
+        
+        buts.add(new Button(1, new Rect(0,0,0,0,Color.black, Color.black)));
+        buts.add(new Button(2, new Ellipse(0,0,0,0,Color.black, Color.black)));
+        buts.add(new Button(3, new Arc(0,0,0,0,Color.black, Color.black, 110, 190)));
+        buts.add(new Button(4, new Trian(25,185,30,30,Color.black, Color.black)));
+        
         this.addMouseListener
         (
             new MouseAdapter()
@@ -40,14 +69,38 @@ Point ponto;
                 {    Point ponto = getMousePosition();
                      int px = ponto.x; 
                      int py = ponto.y;
+                     
                  
                     focus = null;
+                    
+                           Color contorno = new Color(rand.nextInt(255),rand.nextInt(255),rand.nextInt(255));
+                     Color fill = new Color(rand.nextInt(255),rand.nextInt(255),rand.nextInt(255));
+                     
+                    
+                     
+                     if(focus_but != null && !((evt.getX() >= 20 && evt.getX() <= 60 && evt.getY() >= 65 && evt.getY() <= 185))) {
+		                if (focus_but.idx == 1) {
+			                figs.add(new Rect(evt.getX(),evt.getY(), rand.nextInt(50),rand.nextInt(50),contorno, fill));
+						} 
+						else if (focus_but.idx == 2) {
+							figs.add(new Ellipse(evt.getX(),evt.getY(), rand.nextInt(50),rand.nextInt(50),contorno, fill));
+						}
+						else if (focus_but.idx == 3) {
+							figs.add(new Arc(evt.getX(),evt.getY(), rand.nextInt(50),rand.nextInt(50), contorno,fill,rand.nextInt(360),rand.nextInt(360)));
+						}
+						else if (focus_but.idx == 4) {
+							figs.add(new Trian(evt.getX(),evt.getY(), rand.nextInt(50),rand.nextInt(50),contorno, fill));
+						}
+						repaint();
+					}
+                    
+                    
                     for (Figure fig: figs)
                     {
-                        if (fig.clicked(evt.getX(), evt.getY()))    
+                        if (fig.clicked(evt.getX(), evt.getY()))   
                         {   focus = fig;
                             figs.remove(focus);
-			    figs.add(focus);
+						    figs.add(focus);
                             repaint();
                             break;
                         }
@@ -57,19 +110,38 @@ Point ponto;
                             repaint();
                         }
                     }
+                    for (Button but: buts) {
+		               if (but.clicked(px,py)) {
+			                focus_but = but;
+							buts.remove(but);
+                            buts.add(but);
+			                repaint();
+			                break;
+			            }
+						else {
+						    focus_but = null;
+							repaint();
+						}
+		            }
+		     
                 }
+                
+                
             }
         );
         
+        
+		
         this.addMouseMotionListener
         ( 
             new MouseMotionAdapter()
             {
                 public void mouseDragged (MouseEvent evt)
                 {       if (focus != null) 
-                        {   focus.x = evt.getX() - focus.w/2;
-        		    focus.y = evt.getY() - focus.h/2;
-        		    repaint();
+                        {   int dx = (evt.getX() - focus.x)-focus.w/2;
+        			        int dy = (evt.getY() - focus.y) - focus.h/2;
+        			        focus.drag(dx,dy);
+        			        repaint();
                         }
                     }
                 }
@@ -78,8 +150,14 @@ Point ponto;
         this.addKeyListener (
             new KeyAdapter() {
                 public void keyPressed (KeyEvent evt) {
-                    int x = rand.nextInt(350);
-                    int y = rand.nextInt(350);
+                pos = getMousePosition();
+
+                        if (pos == null) {
+                            return;
+                        }
+                        
+                    int x = pos.x;
+                    int y = pos.y;
                     int w = rand.nextInt(50);
                     int h = rand.nextInt(50);
                     int anguloInicial = rand.nextInt(360);
@@ -103,58 +181,84 @@ Point ponto;
                                 focus = null;
                     }
                        if(evt.getKeyChar() == '-'){
-		         for(Figure fig: figs){
+						for(Figure fig: figs){
                              if(focus == fig){
-				fig.w--;
-				fig.h--;
-			      }
-			}
-		       }
+								fig.w--;
+								fig.h--;
+						   	}
+						  }
+					   }
 						 
-			if(evt.getKeyChar() == '+'){
-			  for(Figure fig: figs){
-                              if(focus == fig){
-				fig.w++;
-				fig.h++;
-			       }
-			   }
-			} 
-		        if (evt.getKeyCode() == 37) {
-			  for(Figure fig: figs){
-			      if(focus == fig){
-			        fig.x -= 5;     
-			      }
-			  }
+					   if(evt.getKeyChar() == '+'){
+				        for(Figure fig: figs){
+                             if(focus == fig){
+								fig.w++;
+								fig.h++;
+						   }
+						 }
+					   } 
+					   if (evt.getKeyCode() == 37) {
+					    for(Figure fig: figs){
+					         if(focus == fig){
+					            focus.drag(-5,0);     
+					         }
+					    }
 					   
-			}
+					   }
 					   
-			if (evt.getKeyCode() == 38) {
-			  for(Figure fig: figs){
-			      if(focus == fig){
-			        fig.y -= 5;     
-			      }
-			   }
+					   if (evt.getKeyCode() == 38) {
+					    for(Figure fig: figs){
+					         if(focus == fig){
+					            focus.drag(0,-5); 
+					         }
+					    }
 					   
-			}
+					   }
 					   
-			if (evt.getKeyCode() == 39) {
-			  for(Figure fig: figs){
-			      if(focus == fig){
-				fig.x += 5;     
-			      }
-			  }
+					   if (evt.getKeyCode() == 39) {
+					    for(Figure fig: figs){
+					         if(focus == fig){
+					           focus.drag(5,0); 
+					         }
+					    }
 					   
-			}
+					   }
 					   
-			if (evt.getKeyCode() == 40) {
-			  for(Figure fig: figs){
-			      if(focus == fig){
-			        fig.y += 5;     
-			      }
-			  }
+					   if (evt.getKeyCode() == 40) {
+					    for(Figure fig: figs){
+					         if(focus == fig){
+					            focus.drag(0,5); 
+					         }
+					    }
 					   
-			}
-					
+					   }
+					   if(evt.getKeyChar() == '1' && focus!=null ){
+                        focus.fundo=Color.blue;
+                       }
+					   if(evt.getKeyChar() == '2' && focus!=null ){
+                        focus.fundo=Color.yellow;
+                       }
+                       if(evt.getKeyChar() == '3' && focus!=null ){
+                        focus.fundo=Color.green;
+                       }
+                       if(evt.getKeyChar() == '4' && focus!=null ){
+                        focus.fundo=Color.red;
+                       }
+                       if(evt.getKeyChar() == '5' && focus!=null ){
+                        focus.fundo=Color.orange;
+                       }
+                       if(evt.getKeyChar() == '6' && focus!=null ){
+                        focus.fundo=Color.pink;
+                       }
+                       if(evt.getKeyChar() == '7' && focus!=null ){
+                        focus.fundo=Color.gray;
+                       }
+                       if(evt.getKeyChar() == '8' && focus!=null ){
+                        focus.fundo=Color.black;
+                       }
+                       if(evt.getKeyChar() == '9' && focus!=null ){
+                        focus.fundo=Color.white;
+                       }
                     repaint();
                 }
             }
@@ -167,14 +271,18 @@ Point ponto;
     public void paint (Graphics g) {
         super.paint(g);
         for (Figure fig: this.figs) {
-            fig.paint(g);
+            fig.paint(g, true);
             Graphics2D g2d = (Graphics2D) g;
             
             if (focus != null)
         {       g2d.setColor(Color.RED);
+               
                 g2d.drawRect(focus.x-1, focus.y-1, focus.w+1, focus.h+2);
             }
             
+        }
+        for (Button but: this.buts){
+        but.paint(g, but==focus_but);
         }
         
     }
