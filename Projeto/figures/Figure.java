63a6a0bead5e155685a73e0cc3308ab5f2db57c8
jeanrmoreira <48541470+jeanrmoreira@users.noreply.github.com>
2021-05-30 21:13:59 -0300

    Update Figure.java

--- Projeto/figures/Figure.java
+++ Projeto/figures/Figure.java
@@ -3,8 +3,10 @@ package figures;
 import java.awt.Graphics;
 import java.awt.Color;
 import ivisible.IVisible;
+import java.io.Serializable;
+import ivisible.IVisible;
 
-public abstract class Figure implements IVisible {
+public abstract class Figure implements IVisible, Serializable {
       public int x, y;
       public int w, h;
       public Color contorno, fundo;
@@ -19,7 +21,7 @@ public abstract class Figure implements IVisible {
 public boolean clicked (int x, int y) {
         return (this.x<=x && x<=this.x+this.w && this.y<=y && y<=this.y+this.h);
     }
-    public abstract void paint (Graphics g);
+    public abstract void paint (Graphics g, boolean focused);
     
       
 }
